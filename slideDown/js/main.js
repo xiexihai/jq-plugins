41ffb9fa8861f65bac8fcbfa4bf222cd43675fc1
sweet <2873064616@qq.com>
2016-03-23 21:20:55 +0800

    提交了放大镜、购物车、下拉插件、瀑布流

---
+++ slideDown/js/main.js
@@ -0,0 +1,25 @@
+﻿define(function(require,exports,module){
+	var $=require('jquery');
+	
+	require('slidedown')($);
+	
+	var aLi=$('.nav>li');
+	var aLiWrap=$('.navWrap>li');
+	var aLiWrapBox=$('.navWrapBox>li');
+	aLi.slidedown({
+		
+	});
+	
+	aLiWrapBox.slidedown({
+		'child':'.nav-item',
+		'delay':80
+	});
+	
+	
+	aLiWrap.slidedown({
+		'child':'.nav-item',
+		'event':'click'
+	});
+	
+	
+});
---
+++ slideDown/js/slidedown.js
@@ -0,0 +1,68 @@
+﻿define(function(require,exports,module){
+	
+	return function(jquery){
+	
+		;(function($){
+		
+			var Slidedown=function(element,options){
+				this.element=$(element);
+				this.options=options;
+				this.settings=$.extend({},Slidedown.defaults,this.options);
+				this.timer=null;
+				//this.iStop=true;
+				this.init();
+			}
+			
+			Slidedown.prototype={
+				init:function(){
+					var that=this;
+					this.element.on(that.settings.event,function(){
+						var _this=$(this);
+						_this.addClass(that.settings.on);
+						if(that.settings.event=="mouseenter"){
+						
+								clearTimeout(that.timer);
+								
+								that.timer=setTimeout(function(){
+							
+										_this.find(that.settings.child).slideDown(that.settings.time);
+									
+								},that.settings.delay);
+						
+						}else if(that.settings.event=="click"){
+								_this.find(that.settings.child).slideDown(that.settings.time);	
+						}
+					}).on('mouseleave',function(){
+					
+							var _this=$(this);
+							_this.removeClass(that.settings.on);
+							clearTimeout(that.timer);
+							that.timer=setTimeout(function(){
+								_this.find(that.settings.child).delay(that.settings.delay).slideUp(100);
+							},that.settings.delay);
+						
+					});
+				}
+			}
+			
+			Slidedown.defaults={
+				'child':'.nav-item-container',
+				'event':'mouseenter',
+				'delay':150,
+				'time':200,
+				'on':'on'
+				
+			}
+			
+			$.fn.slidedown=function(options){
+				return this.each(function () {
+					new Slidedown(this,options);
+				})
+			}
+			
+		})(jQuery);
+
+	}
+	
+});
+
